bacbf5eecf6229547f7073f5913d94b7e4b460dd
Pratiksha Nayak <156405979+Taeksha@users.noreply.github.com>
2025-03-31 15:14:53 +0530

    Update controller.js

--- backend/controller.js
+++ backend/controller.js
@@ -1,1 +1,9 @@
 
+const signup= async (req, res) => {
+  const { text } = req.body;
+  const productnew = new Task({ text, completed: false });
+  await productnew.save();
+  res.json(productnew);
+}
+
+module.exports=signup
